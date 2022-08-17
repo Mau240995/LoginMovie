2b08cecb75deccb463e336e74d976bda7efdd585
Gerardo Bautista Castañeda <gerarchicharo37@gmail.com>
2022-08-17 10:55:06 -0500

    commit

--- LoginMovie/MoviesViewController.swift
+++ LoginMovie/MoviesViewController.swift
@@ -16,7 +16,7 @@ class MoviesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        
+        self.collectionViewMovies.dataSource = self
         URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://api.themoviedb.org/3/movie/upcoming?api_key=c7f7d1dc5a6aa58fd2f3602748ad9c64&language=en-US&page=1")!)){
             (data,req,error) in
             do{
